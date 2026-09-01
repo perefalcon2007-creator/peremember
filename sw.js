8cda14c89edb18f08164ad7b0462e91c344b5383
perefalcon2007-creator <perefalcon2007@gmail.com>
2026-09-01 11:09:23 +0100

    Add service worker for caching and notifications

---
+++ sw.js
@@ -0,0 +1,42 @@
+var CACHE_NAME = 'peremember-v1';
+var APP_SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(APP_SHELL).catch(function(){ /* ignore individual failures */ });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(names){
+      return Promise.all(names.filter(function(n){ return n !== CACHE_NAME; }).map(function(n){ return caches.delete(n); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    fetch(event.request).then(function(res){
+      var resClone = res.clone();
+      caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, resClone); });
+      return res;
+    }).catch(function(){
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(event){
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window' }).then(function(list){
+      for(var i=0;i<list.length;i++){ if('focus' in list[i]) return list[i].focus(); }
+      if(self.clients.openWindow) return self.clients.openWindow('/');
+    })
+  );
+});
